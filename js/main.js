78041f270c64c3f37d91901358a0aa54455ee008
marcelaOrellana <marcelapaz.1985@gmail.com>
2017-06-19 14:12:17 -0400

    icono de bici, y inputs entre botones

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-   function initMap() {
+function initMap() {
 
-          var map = new google.maps.Map(document.getElementById("map"), {
+    var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 5,
     center: {lat: -9.1191427, lng: -77.0349046},
     mapTypeControl: false,
     zoomControl: false,
     streetViewControl: false
-  });
+    });
 
-function buscar(){
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+    function buscar(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+        }
+    }
+    document.getElementById("encuentrame").addEventListener("click", buscar);
 
-  }
-}
-document.getElementById("encuentrame").addEventListener("click", buscar);
-
-var latitud, longitud;
+    var latitud, longitud;
 
 var funcionExito = function(posicion){
   latitud = posicion.coords.latitude;
   longitud = posicion.coords.longitude;
-
+   var image = 'http://www.adktrailmap.com/webmap/images/biking.png';
   var miUbicacion = new google.maps.Marker({
     position: {lat: latitud, lng: longitud},
     animacion: google.maps.Animation.DROP,
-    map: map
+    map: map,
+    icon: image
   });
   map.setZoom(17);
   map.setCenter({lat: latitud, lng: longitud});
@@ -71,8 +71,8 @@ var funcionError = function(error){
         this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
         this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
 
-        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+        //this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+        //this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
         this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
       }
 
@@ -107,7 +107,6 @@ var funcionError = function(error){
       };
 
 
-     
       AutocompleteDirectionsHandler.prototype.route = function() {
         if (!this.originPlaceId || !this.destinationPlaceId) {
           return;
